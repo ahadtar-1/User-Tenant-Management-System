0a2672ca8490a297bfea9cf2466629091d059d8b
ahadtar-1 <93494109+ahadtar-1@users.noreply.github.com>
2022-06-03 02:20:40 +0500

    Delete BackendAssesmentCopy directory

--- BackendAssesmentCopy/backend/assesment/asgi.py
+++
@@ -1,9 +0,0 @@
-import uvicorn
-import pydantic
-from app.main import app
-import utils.consumer as consumer 
-
-if __name__ == "__main__":
-	print('compiled:', pydantic.compiled) # Put this in some debug logging
-	#uvicorn.run(app, host="0.0.0.0", port=5050)
-	consumer.consume_events()
