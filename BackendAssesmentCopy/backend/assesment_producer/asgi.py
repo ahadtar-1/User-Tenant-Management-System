57efa15599fe7e097f2767c2e24aea557c1c7d7f
AmmarSaqib <ammarsaqib123@gmail.com>
2022-06-21 20:13:42 +0500

    it works now

--- BackendAssesmentCopy/backend/assesment_producer/asgi.py
+++ BackendAssesmentCopy/backend/assesment_producer/asgi.py
@@ -1,7 +1,9 @@
 import uvicorn
 import pydantic
 from app.main import app
+from utils.producer import producer
 
 if __name__ == "__main__":
-	print('compiled:', pydantic.compiled) # Put this in some debug logging
-	uvicorn.run(app, host="0.0.0.0", port=5030)
+    print("compiled:", pydantic.compiled)  # Put this in some debug logging
+    # print(producer.bootstrap_connected(), flush=True)
+    uvicorn.run(app, host="0.0.0.0", port=5030)
--- BackendAssesmentCopy/backend/assesment_producer/utils/producer.py
+++ BackendAssesmentCopy/backend/assesment_producer/utils/producer.py
@@ -4,23 +4,30 @@ import asyncio
 from kafka import KafkaProducer
 import os
 from random import randint
-#import random
-#import time
-#import uuid
 
-KAFKA_TOPIC = os.getenv('KAFKA_TOPIC')
-KAFKA_BOOTSTRAP_SERVERS = os.getenv('KAFKA_BOOTSTRAP_SERVERS', 'localhost:9092')
+# import random
+# import time
+# import uuid
+
+KAFKA_TOPIC = os.getenv("KAFKA_TOPIC")
+KAFKA_BOOTSTRAP_SERVERS = os.getenv("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092")
 
 producer = KafkaProducer(
-   value_serializer=lambda msg: json.dumps(msg).encode('utf-8'), # we serialize our data to json for efficent transfer
-   bootstrap_servers='localhost:9092', api_version=(0,10))
+    value_serializer=lambda msg: json.dumps(msg).encode(
+        "utf-8"
+    ),  # we serialize our data to json for efficent transfer
+    bootstrap_servers="kafkatwo:9092",
+)
 
-TOPIC_NAME = 'data_topic'
+TOPIC_NAME = "data_topic"
+
 
 def produce_event():
     final_msg = "Process done"
     return final_msg
 
+
 def send_events(data):
-    #data = produce_event()
+    # data = produce_event()
+    print(producer.bootstrap_connected(), flush=True)
     producer.send(TOPIC_NAME, value=data)
