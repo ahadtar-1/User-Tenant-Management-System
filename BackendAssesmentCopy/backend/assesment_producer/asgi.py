aa3d1da7a80654d6adb01bc140537f5f55ff5549
ahadtar-1 <93494109+ahadtar-1@users.noreply.github.com>
2022-06-27 15:06:44 +0500

    Delete BackendAssesmentCopy directory

--- BackendAssesmentCopy/backend/assesment_producer/asgi.py
+++
@@ -1,9 +0,0 @@
-import uvicorn
-import pydantic
-from app.main import app
-from utils.producer import producer
-
-if __name__ == "__main__":
-    print("compiled:", pydantic.compiled)  # Put this in some debug logging
-    # print(producer.bootstrap_connected(), flush=True)
-    uvicorn.run(app, host="0.0.0.0", port=5030)
