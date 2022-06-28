2f01b4526d1fec2f4ec9217994346b80dbe7cb1b
ahadtar-1 <93494109+ahadtar-1@users.noreply.github.com>
2022-06-28 14:55:03 +0500

    Add files via upload

---
+++ BackendAssesmentCopy/backend/assesment/asgi.py
@@ -0,0 +1,66 @@
+import uvicorn
+import pydantic
+import sys
+import asyncio
+import threading
+import json
+from threading import Thread
+from fastapi import Depends, FastAPI, HTTPException, APIRouter, Response
+from app.config import Base, SessionLocal, engine
+from sqlalchemy.orm import Session
+from app.main import app
+import app.schemas as schemas
+from app import models
+import utils.consumer as consumer 
+import utils.dbmanagers.crud as crud 
+
+
+#Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+#Converting json data to python class object
+def jsonObjDecoder(obj):
+    if '__type__' in obj and obj['__type__'] == 'TenantCreate':
+        data=schemas.TenantCreate(obj['tenant_id'], obj['tenant_name'], obj['address'], obj['city'], obj['state'], obj['country'], obj['zip_code'], obj['phone'], obj['web_url'])	
+    if '__type__' in obj and obj['__type__'] == 'UserCreate':
+        data=schemas.UserCreate(obj['user_id'], obj['first_name'], obj['last_name'], obj['department'], obj['designation'], obj['tenant_id'], obj['city'], obj['country'], obj['bio'], obj['social_links'], obj['employee_id'])
+    return data
+
+#Inserting data into consumer database   	
+def FuncTwo(db: Session = Depends(get_db)):
+    print('Consumer Started:', flush=True)	
+    m=consumer.consume_events()
+    #classObj = json.loads(m,object_hook=jsonObjDecoder)
+    #className = (type(m))
+    print("Type",type(m))
+    #if type(m) == 'Tenant':
+        #classObj = crud.get_tenant_by_id(db, tenant_id=m.tenant_id)
+        #if classObj:
+             #raise HTTPException(status_code=400, detail="Record already exists")
+        #try:
+             #crud.create_tenant(db=db, tenant=m)
+        #except Exception as error:
+             #print(error, flush=True)
+    #if className == 'UserCreate':
+         #classObj = crud.get_user_by_id(db, user_id=classObj.user_id)
+         #if classObj:
+              #raise HTTPException(status_code=400, detail="Record already exists")
+         #try:
+             #crud.create_user(db=db, user=classObj)
+         #except Exception as error:
+              #print(error, flush=True)
+    
+if __name__ == "__main__":
+    thread_one = Thread(target=FuncTwo,daemon=True)
+    thread_one.start()
+    uvicorn.run(app, host="0.0.0.0", port=5020)
+
+
+
+
+
---
+++ BackendAssesmentCopy/backend/assesment_producer/app/routes.py
@@ -0,0 +1,100 @@
+from fastapi import Depends, FastAPI, HTTPException, APIRouter, Response
+from sqlalchemy.orm import Session
+
+from app.config import Base, SessionLocal, engine
+import app.schemas as schemas
+import app.models as models
+
+import utils.dbmanagers.crud as crud
+import utils.producer as producer 
+
+router = APIRouter()
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@router.post("/users/")
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_id(db, userid=user.user_id)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Record already exists")   
+    try:
+        producer.send_events(user.schema_json())  
+    except Exception as error:
+        print(error,flush=True)
+    return "User Inserted"
+
+@router.post("/tenants/")
+def create_tenant(tenant: schemas.Tenant, db: Session = Depends(get_db)):
+    db_tenant = crud.get_tenant_by_id(db, tenant_id=tenant.tenant_id)
+    if db_tenant:
+        raise HTTPException(status_code=400, detail="Record already exists")
+    try:
+        producer.send_events(tenant.schema_json())
+    except Exception as error:
+        print(error, flush=True)
+    return "Tenant Inserted"
+
+@router.get("/users/", response_model=schemas.User)
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
+
+@router.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_id(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+@router.get("/tenants/", response_model=schemas.Tenant)
+def read_tenants(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    tenants = crud.get_tenants(db, skip=skip, limit=limit)
+    return tenants
+
+@router.get("/tenants/{tenant_id}", response_model=schemas.Tenant)
+def read_tenant(tenant_id: int, db: Session = Depends(get_db)):
+    db_tenant = crud.get_tenant_by_id(db, tenant_id=tenant_id)
+    if db_tenant is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_tenant
+
+@router.delete('/users/{user_id}', response_model=schemas.User)
+def delete_user_by_id(user_id: int, db: Session = Depends(get_db)):
+     db_user = crud.get_user_by_id(db, user_id=user_id)
+     if db_user is None:
+         raise HTTPException(status_code=404, detail="User not found")
+     crud.delete_user(db=db,user_id=user_id)
+
+@router.delete('/tenants/{tenant_id}', response_model=schemas.Tenant)
+def delete_tenant_by_id(tenant_id: int, db: Session = Depends(get_db)):
+     db_tenant = crud.get_tenant_by_id(db, tenant_id=tenant_id)
+     if db_tenant is None:
+         raise HTTPException(status_code=404, detail="User not found")
+     crud.delete_tenant(db=db,tenant_id=tenant_id)
+
+
+
+
+
+
+
+
+
+#@app.post("/users/{user_id}/items/", response_model=schemas.Item)
+#def create_item_for_user(
+#    user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
+#):
+#    return crud.create_user_item(db=db, item=item, user_id=user_id)
+
+
+#@app.get("/items/", response_model=list[schemas.Item])
+#def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+#    items = crud.get_items(db, skip=skip, limit=limit)
+#    return items
